d0d3f10fef9f5974c084c66426826cba773b6d12
rbaul <4RFVbgt%>
2022-12-27 14:24:55 +0200

    Init commit

--- rate-limit-by-header/src/main/java/com/example/ratelimitbyheader/RateLimitInterceptor.java
+++ rate-limit-by-header/src/main/java/com/example/ratelimitbyheader/RateLimitInterceptor.java
@@ -9,9 +9,6 @@ import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.servlet.HandlerInterceptor;
 
-//import javax.servlet.http.HttpServletRequest;
-//import javax.servlet.http.HttpServletResponse;
-
 @RequiredArgsConstructor
 @Slf4j
 public class RateLimitInterceptor implements HandlerInterceptor {
